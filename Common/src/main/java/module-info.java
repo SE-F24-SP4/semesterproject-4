3157a6934cecae5697ed7c24e39e719139328177
MidnightRocket <git@midnightrocket.dev>
2024-03-22 14:13:13 +0100

    Add exports to Common
    Co-authored-by: AleksKors <112690504+AleksKors@users.noreply.github.com>
    Co-authored-by: FearfulSnow <11558593+FearfulSnow@users.noreply.github.com>
    Co-authored-by: MartinLundDam <116833415+MartinLundDam@users.noreply.github.com>
    Co-authored-by: vimis22 <115539722+vimis22@users.noreply.github.com>

--- Common/src/main/java/module-info.java
+++ Common/src/main/java/module-info.java
@@ -1,2 +1,8 @@
 module Common {
+	exports com.github.sef24sp4.common.data;
+	exports com.github.sef24sp4.common.entities;
+	exports com.github.sef24sp4.common.gamecontrol;
+	exports com.github.sef24sp4.common.interfaces;
+	exports com.github.sef24sp4.common.metadata;
+	exports com.github.sef24sp4.common.services;
 }
