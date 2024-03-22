8dccdd9b02e467166b9a0632c6395b7de2c88fb4
MidnightRocket <git@midnightrocket.dev>
2024-03-22 15:00:28 +0100

    Add CommonEnemy interfaces
    Co-authored-by: AleksKors <112690504+AleksKors@users.noreply.github.com>
    Co-authored-by: FearfulSnow <11558593+FearfulSnow@users.noreply.github.com>
    Co-authored-by: MartinLundDam <116833415+MartinLundDam@users.noreply.github.com>
    Co-authored-by: vimis22 <115539722+vimis22@users.noreply.github.com>

---
+++ CommonEnemy/src/main/java/com/github/sef24sp4/common/enemy/EnemySPI.java
@@ -0,0 +1,11 @@
+package com.github.sef24sp4.common.enemy;
+
+import com.github.sef24sp4.common.entities.IEntity;
+
+/**
+ * Interface not done (stable)
+ */
+public interface EnemySPI {
+
+	public IEntity createEnemy();
+}
--- CommonEnemy/src/main/java/module-info.java
+++ CommonEnemy/src/main/java/module-info.java
@@ -1,2 +1,3 @@
 module CommonEnemy {
+	requires Common;
 }
