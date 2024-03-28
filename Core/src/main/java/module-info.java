ed1ea47490339d3ee983e638f68ba482c006c230
MidnightRocket <git@midnightrocket.dev>
2024-03-28 20:59:22 +0100

    Core add Common as dependency

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module Core {
 	requires javafx.graphics;
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires Common;
 	opens com.github.sef24sp4.core to javafx.fxml, javafx.graphics;
 	opens com.github.sef24sp4.core.scenes to javafx.fxml, javafx.graphics;
 }
