bee3b941763806b127adb04a85361b2e34184df8
MidnightRocket <git@midnightrocket.dev>
2024-03-28 17:19:09 +0100

    Add JavaFX FXML dependency to Maven
    JavaFX FXML is used for Start menu scene.

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module Core {
 	requires javafx.graphics;
-	opens com.github.sef24sp4.core to javafx.graphics;
+	requires javafx.controls;
+	requires javafx.fxml;
+	opens com.github.sef24sp4.core to javafx.graphics, javafx.fxml;
 }
