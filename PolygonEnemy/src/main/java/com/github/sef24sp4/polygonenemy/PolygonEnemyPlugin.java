2f8072471d32fd9eefb73b1357c7ea08a7c69afd
FearfulSnow <11558593+FearfulSnow@users.noreply.github.com>
2024-03-29 17:12:36 +0100

    A Plugin for demo purpose
    Will spawn enemy around the center with a random amount of enemies and edges for those enemies.

---
+++ PolygonEnemy/src/main/java/com/github/sef24sp4/polygonenemy/PolygonEnemyPlugin.java
@@ -0,0 +1,47 @@
+package com.github.sef24sp4.polygonenemy;
+
+import com.github.sef24sp4.common.data.Coordinates;
+import com.github.sef24sp4.common.entities.IEntity;
+import com.github.sef24sp4.common.interfaces.IEntityManager;
+import com.github.sef24sp4.common.interfaces.IGameSettings;
+import com.github.sef24sp4.common.services.IGamePluginService;
+
+import java.util.Random;
+
+// For demo
+public class PolygonEnemyPlugin implements IGamePluginService {
+	@Override
+	public void launch(IEntityManager entityManager, IGameSettings gameSettings) {
+
+	}
+
+	@Override
+	public void gameStart(IEntityManager entityManager, IGameSettings gameSettings) {
+		// spawn random enemies in a circle around the middle with equal spacing
+		Random generator = new Random();
+		int enemyAmount = generator.nextInt(3, 20);
+		for (int enemy = 0; enemy < enemyAmount; enemy++) {
+			// create entity with a random edge between 3 and 10
+			int amountOfEdges = generator.nextInt(3, 10);
+			PolygonEnemy polygonEnemy = new PolygonEnemy(amountOfEdges);
+
+			// find x
+			double x = 20 * Math.cos(2 * Math.PI * enemy / enemyAmount);
+			// find y
+			double y = 20 * Math.sin(2 * Math.PI * enemy / enemyAmount);
+			// add start position
+			Coordinates startPosition = new Coordinates(x,y);
+			polygonEnemy.setCoordinates(startPosition);
+			// add entity
+			entityManager.addEntity(polygonEnemy);
+		}
+	}
+
+	@Override
+	public void gameStop(IEntityManager entityManager, IGameSettings gameSettings) {
+		// delete all enemies of type PolygonEnemy
+		for (IEntity polygonEnemy : entityManager.getEntitiesByClass(PolygonEnemy.class)) {
+			entityManager.removeEntity(polygonEnemy);
+		}
+	}
+}
