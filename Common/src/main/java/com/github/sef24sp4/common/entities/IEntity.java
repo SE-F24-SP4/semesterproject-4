27ff01cb563d87798074ca2ef89f5c712d09f2fe
MidnightRocket <git@midnightrocket.dev>
2024-03-29 20:06:32 +0100

    Add getPolygonValuesArray to entities
    This returns an array of primitive doubles which can be used in the new
    Polygon method for javafx.

--- Common/src/main/java/com/github/sef24sp4/common/entities/IEntity.java
+++ Common/src/main/java/com/github/sef24sp4/common/entities/IEntity.java
@@ -2,11 +2,22 @@ package com.github.sef24sp4.common.entities;
 
 import com.github.sef24sp4.common.data.Coordinates;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface IEntity {
 
 	public List<Coordinates> getPolygonCoordinates();
+
+	public default double[] getPolygonValuesArray() {
+		List<Double> output = new ArrayList<>(this.getPolygonCoordinates().size() * 2);
+		this.getPolygonCoordinates().forEach(coordinates -> {
+			output.add(coordinates.getX());
+			output.add(coordinates.getY());
+		});
+		// See: https://stackoverflow.com/a/23945015
+		return output.stream().mapToDouble(Double::doubleValue).toArray();
+	}
 
 	public double getRotation();
 
