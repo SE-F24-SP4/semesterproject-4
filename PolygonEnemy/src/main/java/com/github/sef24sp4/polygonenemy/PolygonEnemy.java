bf0f075d501db1d1f25e5e6b13d28cc4dfea681d
FearfulSnow <11558593+FearfulSnow@users.noreply.github.com>
2024-03-29 18:03:41 +0100

    Previous bugfix made more general
    Instead of seeing if it collides with other Polygon entites, it now checks for other entites with the same type which is ENEMY

--- PolygonEnemy/src/main/java/com/github/sef24sp4/polygonenemy/PolygonEnemy.java
+++ PolygonEnemy/src/main/java/com/github/sef24sp4/polygonenemy/PolygonEnemy.java
@@ -87,7 +87,9 @@ public class PolygonEnemy extends CommonEntity implements ICollidableEntity, IAt
 
 	@Override
 	public void collide(IEntityManager entityManager, ICollidableEntity otherEntity) {
-		if (otherEntity instanceof IAttackingEntity attackingEntity && attackingEntity.getAttackDamage() > 0 && !(otherEntity instanceof PolygonEnemy)) {
+		if (otherEntity instanceof IAttackingEntity attackingEntity
+				&& attackingEntity.getAttackDamage() > 0
+				&& otherEntity.getMetadata().getType() != this.getMetadata().getType()) {
 			this.health -= attackingEntity.getAttackDamage();
 			// death check
 			if (this.getHealth() < 0) {
