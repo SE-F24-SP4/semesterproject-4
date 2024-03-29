2998b0bc7f3138117c24ea85d74df6150ede3826
alkor22 <alkor22@student.sdu.dk>
2024-03-29 12:45:26 +0100

    Implement player rotation
    Implementet new code to get mouse cordinates and removed old code

--- Player/src/main/java/com/github/sef24sp4/player/PlayerControl.java
+++ Player/src/main/java/com/github/sef24sp4/player/PlayerControl.java
@@ -1,7 +1,6 @@
 package com.github.sef24sp4.player;
 
 import com.github.sef24sp4.common.entities.CommonEntity;
-import com.github.sef24sp4.common.gamecontrol.IMouseCoordinates;
 import com.github.sef24sp4.common.gamecontrol.InputAction;
 import com.github.sef24sp4.common.interfaces.IEntityManager;
 import com.github.sef24sp4.common.interfaces.IGameSettings;
@@ -10,14 +9,16 @@ import com.github.sef24sp4.common.services.IEntityProcessingService;
 public class PlayerControl implements IEntityProcessingService {
 	private double walkSpeed = 10;
 	private double diagonalWalkSpeed = Math.sqrt(2 * (this.walkSpeed * this.walkSpeed));
-	private IMouseCoordinates mouse;
+
 	@Override
 	public void process(IEntityManager entityManager, IGameSettings gameSettings) {
 		for (CommonEntity player : entityManager.getEntitiesByClass(Player.class)) {
-			player.setRotation(this.getRotationToCursor(player, this.mouse));
+			//Set rotation to look a cursor
+			player.getCoordinates().getAngleBetween(gameSettings.getKeys().getMouseCoordinates().getCoordinates());
+			//Check if it should shoot
 			if (gameSettings.getKeys().isDown(InputAction.SHOOT)) {
 				System.out.println("Shooting not implemented yet");
-				//Shoot
+				//Shoot code here
 			}
 			//Player movement
 			if (gameSettings.getKeys().isDown(InputAction.UP) && gameSettings.getKeys().isDown(InputAction.LEFT)) {
@@ -36,16 +37,20 @@ public class PlayerControl implements IEntityProcessingService {
 				player.setX(player.getX() + this.diagonalWalkSpeed);
 				player.setY(player.getY() - this.diagonalWalkSpeed);
 			}
-			if (gameSettings.getKeys().isDown(InputAction.LEFT) && !gameSettings.getKeys().isDown(InputAction.UP) && !gameSettings.getKeys().isDown(InputAction.DOWN)) {
+			if (gameSettings.getKeys().isDown(InputAction.LEFT) && !gameSettings.getKeys().isDown(InputAction.UP)
+					&& !gameSettings.getKeys().isDown(InputAction.DOWN)) {
 				player.setX(player.getX() - this.walkSpeed);
 			}
-			if (gameSettings.getKeys().isDown(InputAction.RIGHT) && !gameSettings.getKeys().isDown(InputAction.UP) && !gameSettings.getKeys().isDown(InputAction.DOWN)) {
+			if (gameSettings.getKeys().isDown(InputAction.RIGHT) && !gameSettings.getKeys().isDown(InputAction.UP)
+					&& !gameSettings.getKeys().isDown(InputAction.DOWN)) {
 				player.setX(player.getX() + this.walkSpeed);
 			}
-			if (gameSettings.getKeys().isDown(InputAction.UP) && !gameSettings.getKeys().isDown(InputAction.LEFT) && !gameSettings.getKeys().isDown(InputAction.RIGHT)) {
+			if (gameSettings.getKeys().isDown(InputAction.UP) && !gameSettings.getKeys().isDown(InputAction.LEFT)
+					&& !gameSettings.getKeys().isDown(InputAction.RIGHT)) {
 				player.setY(player.getY() + this.walkSpeed);
 			}
-			if (gameSettings.getKeys().isDown(InputAction.DOWN) && !gameSettings.getKeys().isDown(InputAction.LEFT) && !gameSettings.getKeys().isDown(InputAction.RIGHT)) {
+			if (gameSettings.getKeys().isDown(InputAction.DOWN) && !gameSettings.getKeys().isDown(InputAction.LEFT)
+					&& !gameSettings.getKeys().isDown(InputAction.RIGHT)) {
 				player.setY(player.getY() - this.walkSpeed);
 			}
 			//Check if player is outside playable area
@@ -62,14 +67,5 @@ public class PlayerControl implements IEntityProcessingService {
 				player.setY(gameSettings.getDisplayHeight() - 1);
 			}
 		}
-	}
-
-	public double getRotationToCursor(CommonEntity entity, IMouseCoordinates cursor) {
-		double xDistance = cursor.getX() - entity.getX();
-		double yDistance = cursor.getY() - entity.getY();
-
-		double rotation = Math.toDegrees(Math.atan2(yDistance, xDistance));
-
-		return  rotation;
 	}
 }
