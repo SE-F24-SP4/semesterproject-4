4404770dd5b499003001329d76b441025adc17ce
FearfulSnow <11558593+FearfulSnow@users.noreply.github.com>
2024-03-29 16:55:01 +0100

    Stable version of EnemySPI
    with javadoc & module setup

--- CommonEnemy/src/main/java/com/github/sef24sp4/common/enemy/EnemySPI.java
+++ CommonEnemy/src/main/java/com/github/sef24sp4/common/enemy/EnemySPI.java
@@ -1,11 +1,17 @@
 package com.github.sef24sp4.common.enemy;
 
 import com.github.sef24sp4.common.entities.IEntity;
-
-/**
- * Interface not done (stable).
- */
+import com.github.sef24sp4.common.data.Coordinates;
 public interface EnemySPI {
-
-	public IEntity createEnemy();
+	/**
+	 * Create a new enemy with the specified difficulty
+	 * <p>
+	 * Note the new enemy does not have any start coordinates and should be provided after, or it will assume the standard values (0,0) provided by {@link Coordinates}
+	 *
+	 * @param difficulty is a positive integer describing the difficulty of the enemy.
+	 * @return An enemy object of type {@link IEntity}
+	 * @throws IllegalArgumentException if a non-positive integer is provided.
+	 * @see IEntity
+	 */
+	public IEntity createEnemy(int difficulty);
 }
