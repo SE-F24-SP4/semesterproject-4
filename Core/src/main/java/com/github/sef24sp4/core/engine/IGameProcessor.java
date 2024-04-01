9c0137abd6bc0ec02560e2af9f161201bc7207fb
MidnightRocket <git@midnightrocket.dev>
2024-04-01 14:19:41 +0200

    Add Core/engine package
    This includes some interfaces for interacting with basic game execution.

---
+++ Core/src/main/java/com/github/sef24sp4/core/engine/IGameProcessor.java
@@ -0,0 +1,11 @@
+package com.github.sef24sp4.core.engine;
+
+public interface IGameProcessor {
+	public void begin();
+
+	public void end();
+
+	public void pause();
+
+	public void resume();
+}
---
+++ Core/src/main/java/com/github/sef24sp4/core/engine/IGameTickExecutor.java
@@ -0,0 +1,29 @@
+package com.github.sef24sp4.core.engine;
+
+
+/**
+ * A GameTick executor asynchronously runs the game tick code for every frame shown.
+ * Depending on the implementation the GameTick may or may not be executed on the calling thread.
+ */
+public interface IGameTickExecutor {
+
+	/**
+	 * Sets the code to be executed every game tick.
+	 *
+	 * @param gameTick The gameTick to be executed.
+	 */
+	public void setGameTick(IGameTick gameTick);
+
+
+	/**
+	 * Starts the GameTickExecutor asynchronously.
+	 */
+	public void start();
+
+	/**
+	 * Stops the GameTickExecutor.
+	 * It cannot be expected that any currently running gameTick is interrupted.
+	 * This will depend on the implementation.
+	 */
+	public void stop();
+}
