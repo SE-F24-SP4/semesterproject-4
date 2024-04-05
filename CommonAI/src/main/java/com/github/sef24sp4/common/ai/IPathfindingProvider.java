68cd1ed8ed4b5729fe9ae5432b56507b26a18f1d
MartinLundDam <martinlunddam@hotmail.dk>
2024-04-05 11:01:51 +0200

    Java Doc changes

--- CommonAI/src/main/java/com/github/sef24sp4/common/ai/IPathfindingProvider.java
+++ CommonAI/src/main/java/com/github/sef24sp4/common/ai/IPathfindingProvider.java
@@ -10,9 +10,9 @@ public interface IPathfindingProvider {
 	 * @param entity which is an enemy, should go to the
 	 * @param targetCoordinate
 	 * @return an IVector, that is the next node (targetCoordinate), that the entity should go to.
-	 *         This can be a coordinate.
+	 *         This can be a coordinate, which is the Coordinate of the player.
 	 */
-	public IVector calculateNextMove(IEntity entity, IVector targetCoordinate); //goalX and goalY beeing position of player.
+	public IVector calculateNextMove(IEntity entity, IVector targetCoordinate);
 
 }
 
