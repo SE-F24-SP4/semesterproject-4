fbe598b8cd02fafee1fcb75d60295940786fa890
MartinLundDam <martinlunddam@hotmail.dk>
2024-04-05 10:56:50 +0200

    Java Doc and Method calculateNextMove, in the IPathfindingProvider interface

--- CommonAI/src/main/java/com/github/sef24sp4/common/ai/IPathfindingProvider.java
+++ CommonAI/src/main/java/com/github/sef24sp4/common/ai/IPathfindingProvider.java
@@ -1,7 +1,19 @@
 package com.github.sef24sp4.common.ai;
 
+import com.github.sef24sp4.common.entities.IEntity;
+import com.github.sef24sp4.common.interfaces.IVector;
+
 public interface IPathfindingProvider {
 
-
+	/**
+	 *
+	 * @param entity which is an enemy, should go to the
+	 * @param targetCoordinate
+	 * @return an IVector, that is the next node (targetCoordinate), that the entity should go to.
+	 *         This can be a coordinate.
+	 */
+	public IVector calculateNextMove(IEntity entity, IVector targetCoordinate); //goalX and goalY beeing position of player.
 
 }
+
+
