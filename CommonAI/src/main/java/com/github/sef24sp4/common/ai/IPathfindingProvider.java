ad48aca4d421412e3c19b3bb3fc4d032292585a0
MartinLundDam <martinlunddam@hotmail.dk>
2024-04-05 12:00:10 +0200

    Java Doc changes

--- CommonAI/src/main/java/com/github/sef24sp4/common/ai/IPathfindingProvider.java
+++ CommonAI/src/main/java/com/github/sef24sp4/common/ai/IPathfindingProvider.java
@@ -6,11 +6,11 @@ import com.github.sef24sp4.common.interfaces.IVector;
 public interface IPathfindingProvider {
 
 	/**
+	 * Entity can by implementing this method navigtate to a TargatCoordinate.
+	 * The Method start from the StartCoordinate of the Entity, open neighbor nodes, and expand the node with the lowest fCost.
+	 * When the best route towards a target is known. The Entity will navigate to the next step on this route, which is the TargatCoordinate.
 	 *
-	 * @param entity which is an enemy, should go to the
-	 * @param targetCoordinate
-	 * @return an IVector, that is the next node (targetCoordinate), that the entity should go to.
-	 *         This can be a coordinate, which is the Coordinate of the player.
+	 * @return an IVector, that is the next node (targetCoordinate), that the entity should go to. This can be a coordinate.
 	 */
 	public IVector calculateNextMove(IEntity entity, IVector targetCoordinate);
 
