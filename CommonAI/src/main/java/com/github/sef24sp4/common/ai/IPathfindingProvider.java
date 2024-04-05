6708b026223830a5e5e2357d84eadd1b81903c83
MartinLundDam <martinlunddam@hotmail.dk>
2024-04-05 13:29:21 +0200

    Javadoc changes

--- CommonAI/src/main/java/com/github/sef24sp4/common/ai/IPathfindingProvider.java
+++ CommonAI/src/main/java/com/github/sef24sp4/common/ai/IPathfindingProvider.java
@@ -8,9 +8,9 @@ public interface IPathfindingProvider {
 	/**
 	 * Entity can by implementing this method navigtate to a TargatCoordinate.
 	 * The Method start from the StartCoordinate of the Entity, open neighbor nodes, and expand the node with the lowest fCost.
-	 * When the best route towards a target is known. The Entity will navigate to the next step on this route, which is the TargatCoordinate.
-	 * @param entity is for now a PolygonEnemy.
-	 * @param targetCoordinate is the next step for PolygonEnemy to go to.
+	 * When the best route towards a target is known, the Entity will navigate to the next step on this route towards the TargatCoordinate.
+	 * @param entity is the entity that needs to be moved.
+	 * @param targetCoordinate is the Coordinate the entity needs to go to.
 	 * @return an IVector, that is the next node (targetCoordinate), that the entity should go to. This can be a coordinate.
 	 */
 	public IVector calculateNextMove(IEntity entity, IVector targetCoordinate);
