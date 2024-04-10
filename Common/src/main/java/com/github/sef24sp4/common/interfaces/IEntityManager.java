52f8ef4ad133ca60dcc606497833b8cb4f8bee57
MidnightRocket <git@midnightrocket.dev>
2024-04-10 20:57:38 +0200

    Change IEntityManager to return Collection and prevent duplicates
    Changed to use the Collection interface instead of List, as it is more
    generic, and encompasses multiple types of collections, such as sets.
    
    Changed so that the underlying implementation uses HashSet.
    
    Issue: #78

--- Common/src/main/java/com/github/sef24sp4/common/interfaces/IEntityManager.java
+++ Common/src/main/java/com/github/sef24sp4/common/interfaces/IEntityManager.java
@@ -2,23 +2,55 @@ package com.github.sef24sp4.common.interfaces;
 
 import com.github.sef24sp4.common.entities.IEntity;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 
+/**
+ * An EntityManager used to keep track of all active {@link IEntity entities} in the game.
+ */
 public interface IEntityManager {
 
-	public boolean addEntity(IEntity entity);
+	/**
+	 * Add {@link IEntity} to the EntityManager if not already present in the manager.
+	 * This means there may never be duplicate entities within the same manager.
+	 * Specifically that no two objects ({@code e}, {@code e2}) exists such that {@code e.equals(e2)} is {@code true}.
+	 *
+	 * @param entity The {@link IEntity entity} to be added.
+	 * @return {@code true} if manager did not already contain {@code entity}, and the {@code entity} was added successfully.
+	 * {@code false} if the manager already contained the {@code entity}.
+	 */
+	public boolean addEntity(final IEntity entity);
 
-	public boolean removeEntity(IEntity entity);
+	/**
+	 * Removes the {@code entity} from the manager if present, according to {@link Object#equals entity.equals()} method.
+	 *
+	 * @param entity The {@link IEntity entity} to be removed.
+	 * @return {@code true} if {@code entity} was present and successfully removed.
+	 * {@code false} if the manager did not contain the {@code entity}.
+	 */
+	public boolean removeEntity(final IEntity entity);
 
-	public List<IEntity> getAllEntities();
+	/**
+	 * Get a {@link Collections#unmodifiableCollection read-only Collection} with all entities contained within the manager.
+	 *
+	 * @return A {@link Collections#unmodifiableCollection read-only Collection} with the entities.
+	 */
+	public Collection<IEntity> getAllEntities();
 
-	public default <E extends IEntity> List<E> getEntitiesByClass(Class<E> entityType) {
-		List<E> output = new ArrayList<>();
+	/**
+	 * Get a {@link Collections#unmodifiableCollection read-only Collection} containing all entities from this manager of the {@code entityType}.
+	 *
+	 * @param entityType A {@link Class} for the type of entities to retrieve.
+	 * @param <E>        The entity type.
+	 * @return A {@link Collections#unmodifiableCollection read-only Collection} with the entities of {@code entityType}.
+	 */
+	public default <E extends IEntity> Collection<E> getEntitiesByClass(final Class<E> entityType) {
+		final Collection<E> output = new HashSet<>();
 		this.getAllEntities().forEach((entity) -> {
 			if (entityType.isInstance(entity)) output.add(entityType.cast(entity));
 		});
-		return output;
+		return Collections.unmodifiableCollection(output);
 	}
 
 	/**
@@ -26,10 +58,10 @@ public interface IEntityManager {
 	 *
 	 * @param entityType The type of entities to remove.
 	 * @param <E>        The entity type.
-	 * @return A {@link List} of the removed entities.
+	 * @return A {@link Collection} of the removed entities.
 	 */
-	public default <E extends IEntity> List<E> removeEntitiesByClass(Class<E> entityType) {
-		List<E> entities = this.getEntitiesByClass(entityType);
+	public default <E extends IEntity> Collection<E> removeEntitiesByClass(final Class<E> entityType) {
+		final Collection<E> entities = this.getEntitiesByClass(entityType);
 		entities.forEach(this::removeEntity);
 		return entities;
 	}
