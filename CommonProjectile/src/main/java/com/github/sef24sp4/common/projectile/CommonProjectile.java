fa10e9559d7abf28a9f66fd379d31f37e948982b
MidnightRocket <git@midnightrocket.dev>
2024-04-10 11:44:02 +0200

    Change requirements for Shooter to IEntity
    This enables more flexibility in who is able to shoot projectiles in the
    future.

--- CommonProjectile/src/main/java/com/github/sef24sp4/common/projectile/CommonProjectile.java
+++ CommonProjectile/src/main/java/com/github/sef24sp4/common/projectile/CommonProjectile.java
@@ -2,15 +2,16 @@ package com.github.sef24sp4.common.projectile;
 
 import com.github.sef24sp4.common.entities.CommonEntity;
 import com.github.sef24sp4.common.entities.ICollidableEntity;
+import com.github.sef24sp4.common.entities.IEntity;
 import com.github.sef24sp4.common.metadata.GameElementType;
 import com.github.sef24sp4.common.metadata.IGameMetadata;
 import com.github.sef24sp4.common.metadata.MetadataBuilder;
 
 public abstract class CommonProjectile extends CommonEntity implements ICollidableEntity {
 	private final IGameMetadata metadata;
-	private final ICollidableEntity shooter;
+	private final IEntity shooter;
 
-	protected CommonProjectile(final ICollidableEntity shooter) {
+	protected CommonProjectile(final IEntity shooter) {
 		this.metadata = new MetadataBuilder(GameElementType.PROJECTILE).
 				getMetadata();
 		this.shooter = shooter;
@@ -21,7 +22,7 @@ public abstract class CommonProjectile extends CommonEntity implements ICollidab
 	 *
 	 * @return The entity who shot the projectile.
 	 */
-	public final ICollidableEntity getShooter() {
+	public final IEntity getShooter() {
 		return this.shooter;
 	}
 
--- CommonProjectile/src/main/java/com/github/sef24sp4/common/projectile/ProjectileSPI.java
+++ CommonProjectile/src/main/java/com/github/sef24sp4/common/projectile/ProjectileSPI.java
@@ -1,8 +1,8 @@
 package com.github.sef24sp4.common.projectile;
 
-import com.github.sef24sp4.common.entities.ICollidableEntity;
+import com.github.sef24sp4.common.entities.IEntity;
 
 public interface ProjectileSPI {
 
-	public CommonProjectile createProjectile(ICollidableEntity shooter);
+	public CommonProjectile createProjectile(IEntity shooter);
 }
