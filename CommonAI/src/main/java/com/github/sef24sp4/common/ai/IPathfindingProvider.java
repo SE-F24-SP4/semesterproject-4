d39762bec31216a5d46f76175690f8b3c2927b57
MartinLundDam <martinlunddam@hotmail.dk>
2024-04-10 10:22:39 +0200

    Java doc changes

--- CommonAI/src/main/java/com/github/sef24sp4/common/ai/IPathfindingProvider.java
+++ CommonAI/src/main/java/com/github/sef24sp4/common/ai/IPathfindingProvider.java
@@ -6,8 +6,8 @@ import com.github.sef24sp4.common.interfaces.IVector;
 public interface IPathfindingProvider {
 
 	/**
-	 * Entity can by implementing this method navigate to a TargatCoordinate.
-	 * The Method gives the entity the next coordinate on the optimal route towards the TargatCoordinate.
+	 * Entity can by implementing this method navigate to a TargetCoordinate.
+	 * The Method gives the entity the next coordinate on the optimal route towards the TargetCoordinate.
 	 * @param entity is the entity that needs to be moved.
 	 * @param targetCoordinate is the Coordinate the entity essentially needs to go to.
 	 * @return an IVector, that is the next coordinate that the entity should go to.
