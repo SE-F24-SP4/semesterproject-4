caa3c0427dcca15ebb02932ee2bdbae9d1d153cd
MartinLundDam <martinlunddam@hotmail.dk>
2024-04-11 14:36:57 +0200

    changes from int to double.

--- AStarAI/src/main/java/com/github/sef24sp4/astarai/AStar.java
+++ AStarAI/src/main/java/com/github/sef24sp4/astarai/AStar.java
@@ -6,6 +6,10 @@ import com.github.sef24sp4.common.entities.IEntity;
 import com.github.sef24sp4.common.interfaces.IVector;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static java.lang.Double.MAX_VALUE;
 
 public class AStar implements IPathfindingProvider {
 	private Node[][] node;
@@ -15,9 +19,9 @@ public class AStar implements IPathfindingProvider {
 
 	private boolean goalReached = false;
 
-	private ArrayList<Node> openList = new ArrayList<>();
-	private ArrayList<Node> closedList = new ArrayList<>();
-	private ArrayList<Node> pathList = new ArrayList<>();
+	private List<Node> openList = new ArrayList<>();
+	private List<Node> closedList = new ArrayList<>();
+	private List<Node> pathList = new ArrayList<>(); //this should be a PriorityQueue
 
 	//Explaination
 	//startNode is where the entity using AI is.
@@ -31,45 +35,47 @@ public class AStar implements IPathfindingProvider {
 	@Override
 	public IVector nextCoordinateInPath(IEntity entity, IVector targetCoordinate) {
 		//set goalNode based on targetCoordinate
-		int targetX = (int) targetCoordinate.getX();//does it cause problems that it casts to int?
-		int targetY = (int) targetCoordinate.getY();
+		int targetX = (int) Math.round(targetCoordinate.getX()); //round to nearest int (so 3.9 is 4 instead of 3)
+		int targetY = (int) Math.round(targetCoordinate.getY());
 		this.goalNode = node[targetX][targetY]; //does this work?
 
 		//set startNode based on entity
-		int startX = (int) entity.getX();
-		int startY = (int) entity.getY();
+		int startX = (int) Math.round(entity.getX());
+		int startY = (int) Math.round(entity.getY());
 		this.startNode = node[startX][startY];
 
-		boolean pathFound = search(); //return true if path is found
+		search();
 
-		if (pathFound) { //can be exchanged with just if search
-			Node nextStep = getNextStep();
-			double nextStepX = nextStep.getX();
-			double nextStepY = nextStep.getY();
-
-			return new Coordinates(nextStepX,nextStepY);
+		Optional<Node> nextStep = getNextStep();
+		if (nextStep.isPresent()) {
+			Node nextNode = nextStep.get();
+			return new Coordinates(nextNode.getX(), nextNode.getY());
 		}
-
-		return null;
+		return entity.getCoordinates();
 	}
 
-	public Node getNextStep() { //returns the next Node in the pathList
-		if  (!this.pathList.isEmpty()) {
-			return pathList.remove(0);
+	public Optional<Node> getNextStep() { //returns the next Node in the pathList
+		if (!this.pathList.isEmpty()) {
+			return Optional.of(pathList.remove(0));
 		}
-		return null;
+		return Optional.empty();
 	}
 
 	//TODO: Calculate costs
 	public void getCost(Node aNode) {
-		//gCost
-
+		//gCost (maybe it should be all parent nodes to startnode to get accurate cost)
+		double gCostX = (double) aNode.getX() - (double) this.startNode.getX();
+		double gCostY = (double) aNode.getY() - (double) this.startNode.getY();
+		double gDistance = Math.sqrt(gCostX * gCostX + gCostY * gCostY);
+		aNode.setGCost(gDistance);
 		//hCost
-
+		double hCostX = (double) aNode.getX() - (double) this.goalNode.getX();
+		double hCostY = (double) aNode.getY() - (double) this.goalNode.getY();
+		double hDistance = Math.sqrt(hCostX * hCostX + hCostY * hCostY);
 		//fCost
+		aNode.setFCost(gDistance + hDistance);
 	}
-
-
+	/*
 	public boolean search() { //could i add parameter nodes?
 		//at first current node is the same as startnode
 		this.currentNode = this.startNode;
@@ -84,13 +90,109 @@ public class AStar implements IPathfindingProvider {
 
 			this.openList.remove(this.currentNode);
 
-			//open left neighbor
-			if (x - 1 >= 0) { //is this correct?
-				this.openNode(this.node[x - 1][y]);
+			checkNeighborNodes(currentNode);
+
+			int bestNode = 0; //best node in index
+			double bestFCost = MAX_VALUE; //so far best fCost close to infinity.
+
+			//find best node with loop though openList, comparing fCost.
+			for (int i = 0; i < this.openList.size(); i++) {
+				getCost(this.openList.get(i)); //calculate its cost.
+				if (this.openList.get(i).getFCost() < bestFCost) {
+					bestNode = i;
+					bestFCost = this.openList.get(i).getFCost();
+				}
+				else if (this.openList.get(i).getFCost() == bestFCost) {
+					if (this.openList.get(i).getGCost() < openList.get(bestNode).getGCost()){
+						bestNode = i;
+					}
+				}
 			}
-			//Expand to see other
-			//TODO: Check if neighbor nodes are on the map.
-/*
+			//If there is no elements in openlist, end loop.
+			if (this.openList.isEmpty()) {
+				break;
+			}
+
+			this.currentNode = this.openList.get(bestNode);
+
+			if (this.currentNode.equals(this.goalNode)) {
+				this.goalReached = true;
+
+				//when goal reached : track the path by adding all parent node from goalNode to startNode.
+				trackPath();
+			}
+			//step++? to avoid it calculates the whole way, when enemy is far away.
+			//Could be based on the distance of Heuristics.
+
+		}
+		return this.goalReached; //return if goal is reached or not.
+	}
+
+	 */
+
+	public void search() { //could i add parameter nodes?
+		//at first current node is the same as startnode
+		this.currentNode = this.startNode;
+
+		while (!this.goalReached) { // and step =?
+
+			this.currentNode.setChecked(true);
+			this.openList.remove(this.currentNode); //what if it is not in list?
+
+			checkNeighborNodes(currentNode);//neighbors added to openlist if possible.
+
+			int bestNode = 0; //best node in index
+			double bestFCost = MAX_VALUE; //so far best fCost close to infinity.
+
+			//find best node with loop though openList, comparing fCost.
+			for (int i = 0; i < this.openList.size(); i++) {
+				getCost(this.openList.get(i)); //calculate its cost.
+				if (this.openList.get(i).getFCost() < bestFCost) {
+					bestNode = i;
+					bestFCost = this.openList.get(i).getFCost();
+				} else if (this.openList.get(i).getFCost() == bestFCost) {
+					if (this.openList.get(i).getGCost() < openList.get(bestNode).getGCost()) {
+						bestNode = i;
+					}
+				}
+			}
+
+			if (this.openList.isEmpty()) {
+				break;
+			}
+
+			this.currentNode = this.openList.get(bestNode); //next curentNode is the one with lowest fCost.
+
+			if (this.currentNode.equals(this.goalNode)) {
+				this.goalReached = true;
+				trackPath();
+				return;
+			}
+		}
+	}
+
+	public void trackPath() { //This method can be used to draw and track the path from goalNode to startNode.
+		Node currentNode = this.goalNode;
+
+		while (currentNode != this.startNode) {
+			this.pathList.add(0, currentNode);
+			currentNode = currentNode.getParent(); //parent to currentnode, is the next on path.
+
+			//TODO: track the path with color?
+		}
+	}
+
+	private void checkNeighborNodes(Node theCurrentNode) {
+		//open left neighbor
+		int x = theCurrentNode.getX();
+		int y = theCurrentNode.getY();
+
+		if (x - 1 >= 0) { //is this correct?
+			this.openNode(this.node[x - 1][y]);
+		}
+
+		//TODO: Check if neighbor nodes are on the map.
+
 			//open right neighbor
 			if (x + 1 < gridSizeX) {
 				this.openNode(this.node[x + 1][y]);
@@ -119,49 +221,6 @@ public class AStar implements IPathfindingProvider {
 			if (x + 1 < gridSizeX && y + 1 < gridSizeY) {
 				this.openNode(this.node[x + 1][y + 1]);
 			}
- */
-
-			int bestNode = 0;
-			int bestFCost = 999;
-
-			//find best node with loop though openList, comparing fCost.
-			for (int i = 0; i < this.openList.size(); i++) {
-				if (this.openList.get(i).getFCost() < bestFCost) {
-					bestNode = i;
-					bestFCost = this.openList.get(i).getFCost();
-				}
-				//TODO: if f cost is equal, compare gcost.
-			}
-			//If there is no elements in openlist, end loop.
-			if (this.openList.isEmpty()) {
-				break;
-			}
-
-			this.currentNode = this.openList.get(bestNode);
-
-			if (this.currentNode.equals(this.goalNode)) {
-				this.goalReached = true;
-
-				//when goal reached : track the path by adding all parent node from goalNode to startNode.
-				trackPath();
-			}
-			//step++? to avoid it calculates the whole way, when enemy is far away.
-			//Could be based on the distance of Heuristics.
-
-		}
-		return this.goalReached; //return if goal is reached or not.
-	}
-
-
-	public void trackPath() { //This method can be used to draw and track the path from goalNode to startNode.
-		Node currentNode = this.goalNode;
-
-		while (currentNode != this.startNode) {
-			this.pathList.add(0, currentNode); //does it always add to index 0?
-			currentNode = currentNode.getParent(); //parent to currentnode, is the next on path.
-
-			//TODO: track the path with color?
-		}
 	}
 
 	//Method for open nodes if: not open, not checked and not solid : open it.
@@ -171,7 +230,7 @@ public class AStar implements IPathfindingProvider {
 
 			aNode.setOpen(true);
 
-			aNode.setParent(currentNode); //set currentnode as parent
+			aNode.setParent(this.currentNode); //set currentnode as parent
 
 			this.openList.add(aNode);
 
@@ -199,15 +258,15 @@ public class AStar implements IPathfindingProvider {
 		return this.goalReached;
 	}
 
-	public ArrayList<Node> getOpenList() {
+	public List<Node> getOpenList() {
 		return this.openList;
 	}
 
-	public ArrayList<Node> getClosedList() {
+	public List<Node> getClosedList() {
 		return this.closedList;
 	}
 
-	public ArrayList<Node> getPathList() {
+	public List<Node> getPathList() {
 		return this.pathList;
 	}
 
