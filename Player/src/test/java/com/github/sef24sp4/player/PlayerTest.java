25821e03c7fab24e4fafcc4b5d5585b382447276
alkor22 <alkor22@student.sdu.dk>
2024-04-12 20:46:11 +0200

    Added tests for player class
    - Still missing tests, but need heal() to be implemented

--- Player/src/test/java/com/github/sef24sp4/player/PlayerTest.java
+++ Player/src/test/java/com/github/sef24sp4/player/PlayerTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class PlayerTest {
 	private Player p;
 	private final IEntityManager em = new EntityManager();
@@ -24,10 +27,13 @@ public class PlayerTest {
 	void getPlayer() {
 		assertEquals(p, Player.getPlayer());
 	}
+	/**
+	 * Need to implement healing
 	@Test
 	void getHealth() {
 		assertEquals(p.getMaxHealth(), p.getHealth());
 	}
+	 **/
 	@Test
 	void setWalkSpeed() {
 		p.setWalkSpeed(100);
@@ -38,7 +44,21 @@ public class PlayerTest {
 	@Test
 	void takeDamage() {
 		assertEquals(p.getMaxHealth(), p.getHealth());
-		p.takeDamage(1, em);
-		assertEquals(p.getMaxHealth()-1, p.getHealth());
+		p.takeDamage(5, em);
+		assertEquals(p.getMaxHealth()-5, p.getHealth());
+	}
+	@Test
+	void kill() {
+		em.addEntity(p);
+		assertEquals(1, em.getAllEntities().stream().filter(e -> e instanceof Player).toList().size());
+		p.kill(em);
+		assertEquals(0, em.getAllEntities().stream().filter(e -> e instanceof Player).toList().size());
+	}
+	@Test
+	void takeDamageAndKill() {
+		em.addEntity(p);
+		assertEquals(1, em.getAllEntities().stream().filter(e -> e instanceof Player).toList().size());
+		p.takeDamage(1000, em);
+		assertEquals(0, em.getAllEntities().stream().filter(e -> e instanceof Player).toList().size());
 	}
 }
