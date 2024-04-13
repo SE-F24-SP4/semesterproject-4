414339946fc1bcdd6243afa8b7deca636860d6b0
FearfulSnow <11558593+FearfulSnow@users.noreply.github.com>
2024-04-13 17:41:41 +0200

    Enemy movement & Player export on module
    Enemy moves towards player if no pathfindingProvider is present

--- PolygonEnemy/src/main/java/com/github/sef24sp4/polygonenemy/PolygonEnemyControlSystem.java
+++ PolygonEnemy/src/main/java/com/github/sef24sp4/polygonenemy/PolygonEnemyControlSystem.java
@@ -1,17 +1,46 @@
 package com.github.sef24sp4.polygonenemy;
 
+import com.github.sef24sp4.common.ai.IPathfindingProvider;
+import com.github.sef24sp4.common.data.Coordinates;
 import com.github.sef24sp4.common.enemy.EnemySPI;
 import com.github.sef24sp4.common.entities.IEntity;
 import com.github.sef24sp4.common.interfaces.IEntityManager;
 import com.github.sef24sp4.common.interfaces.IGameSettings;
+import com.github.sef24sp4.common.interfaces.IVector;
 import com.github.sef24sp4.common.services.IEntityProcessingService;
+import com.github.sef24sp4.player.Player;
+
+import java.util.ServiceLoader;
 
 public class PolygonEnemyControlSystem implements IEntityProcessingService, EnemySPI {
+	private final ServiceLoader<IPathfindingProvider> pathfindingProviders = ServiceLoader.load(IPathfindingProvider.class);
+
 	@Override
 	public void process(IEntityManager entityManager, IGameSettings gameSettings) {
-//		for (IEntity polygonEnemy : entityManager.getEntitiesByClass(PolygonEnemy.class)) {
-//			// movement
-//		}
+		entityManager.getEntitiesByClass(PolygonEnemy.class).forEach( polygonEnemy -> {
+			// standard value, so will always go towards the player.
+			IVector pointToGo = Player.getPlayer().getCoordinates();
+			// will overwrite if pathfinding provider is available, to find a better path to player.
+			if (pathfindingProviders.findFirst().isPresent()) {
+				pointToGo = pathfindingProviders.findFirst().get().nextCoordinateInPath(polygonEnemy, pointToGo);
+			}
+
+			IVector directionVector = polygonEnemy.getCoordinates().getVectorTo(pointToGo);
+
+			// check if the enemy is already at the target position to avoid division by zero.
+			double norm = directionVector.getNorm();
+			if (norm > 0) {
+				// normalize and scale the direction vector
+				IVector movementVector = new Coordinates(
+						(directionVector.getX() / norm) * polygonEnemy.getSpeed(), // Normalize then scale
+						(directionVector.getY() / norm) * polygonEnemy.getSpeed() // Normalize then scale
+				);
+
+				// move enemyPolygon a certain distance depending on movementVector.
+				polygonEnemy.setX(polygonEnemy.getX() + movementVector.getX());
+				polygonEnemy.setY(polygonEnemy.getY() + movementVector.getY());
+			}
+		});
 	}
 
 	@Override
--- PolygonEnemy/src/main/java/module-info.java
+++ PolygonEnemy/src/main/java/module-info.java
@@ -1,11 +1,16 @@
+import com.github.sef24sp4.common.services.IEntityProcessingService;
 import com.github.sef24sp4.common.services.IGamePluginService;
 import com.github.sef24sp4.polygonenemy.PolygonEnemyPlugin;
 import com.github.sef24sp4.common.enemy.EnemySPI;
 import com.github.sef24sp4.polygonenemy.PolygonEnemyControlSystem;
 
 module PolygonEnemy {
+	uses com.github.sef24sp4.common.ai.IPathfindingProvider;
 	requires Common;
 	requires CommonEnemy;
+	requires Player;
+	requires CommonAI;
 	provides IGamePluginService with PolygonEnemyPlugin;
 	provides EnemySPI with PolygonEnemyControlSystem;
+	provides IEntityProcessingService with PolygonEnemyControlSystem;
 }
