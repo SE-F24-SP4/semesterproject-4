e68165f7e93fa3b9161eaf6c81c7c68ac95bc363
MartinLundDam <martinlunddam@hotmail.dk>
2024-04-17 10:53:55 +0200

    Astar Test

--- AStarAI/src/test/java/com/github/sef24sp4/astarai/AStarTest.java
+++ AStarAI/src/test/java/com/github/sef24sp4/astarai/AStarTest.java
@@ -2,40 +2,63 @@ package com.github.sef24sp4.astarai;
 
 import com.github.sef24sp4.common.data.Coordinates;
 import com.github.sef24sp4.common.entities.CommonEntity;
+import com.github.sef24sp4.common.gamecontrol.IGameInput;
 import com.github.sef24sp4.common.interfaces.IGameSettings;
 import com.github.sef24sp4.common.interfaces.IVector;
+import com.github.sef24sp4.core.game.GameSettings;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
 public class AStarTest {
-	private IGameSettings gameSettings;
+
+	private IGameSettings gameSettings = mock(IGameSettings.class);
+	//private GameSettings gs = mock(GameSettings.class);
 
 	@Test
 	void aStarNavigateTest() {
+
 		int mapWidth = 10;
 		int mapHeight = 10;
 
-		this.gameSettings.setDisplayWidth(mapWidth);
+		//when(gameSettings.setDisplayHeight(anyInt())).thenReturn());
+
+		//doAnswer(when(gameSettings).setDisplayHeight(anyInt()));
+
+		//this.gameSettings.setDisplayWidth(mapWidth);
 		this.gameSettings.setDisplayHeight(mapHeight);
+		System.out.println(gameSettings.getDisplayHeight());
 
-		Node[][] nodes = new Node[this.gameSettings.getDisplayWidth()][this.gameSettings.getDisplayHeight()];
+		//assertTrue(this.gameSettings.getDisplayWidth() == 10);
 
-		for (int i = 0; i < mapWidth; i++) {
-			for (int j = 0; j < mapHeight; j++) {
-				nodes[i][j] = new Node(i, j);
+		Node[][] nodes = new Node[10][10];
+
+
+		for (int i = 0; i < mapWidth-1; i++) {
+			for (int j = 0; j < mapHeight-1; j++) {
+				nodes[i][j] = new Node(i,j);
 
 				if (i == 3 && j >= 3 && j <= 7) {
 					nodes[i][j].setSolid(true);
 				}
 			}
 
-			AStar aStar = new AStar();
+
+			//AStar aStar = new AStar();
 			CommonEntity enemy = new CommonEntity();
 			enemy.setX(1);
 			enemy.setY(1);
 
 			IVector iVector = new Coordinates(8, 8);
 
-			aStar.nextCoordinateInPath(enemy, iVector);
+
+			//aStar.nextCoordinateInPath(enemy, iVector);
 		}
+
 	}
+
+
 }
+
