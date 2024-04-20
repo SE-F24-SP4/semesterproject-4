4ec34888fa8d230d92fc03f0f38e9ea3c3e6494d
alkor22 <alkor22@student.sdu.dk>
2024-04-20 14:12:17 +0200

    Changes to ItemSPI
    - Added ItemRarity.
    - Modified ItemSPI to only contain a getRarity() and getItem().

--- CommonItem/src/main/java/com/github/sef24sp4/common/item/ItemSPI.java
+++ CommonItem/src/main/java/com/github/sef24sp4/common/item/ItemSPI.java
@@ -1,26 +1,15 @@
 package com.github.sef24sp4.common.item;
 
-import com.github.sef24sp4.common.interfaces.IEntityManager;
-import com.github.sef24sp4.common.vector.IVector;
-
-
 public interface ItemSPI {
 	/**
-	 * Spawns a new item with specified metadata.
-	 * <p>
-	 * Note the new item does not have any start coordinates and should be provided after,
-	 * or it will assume the standard values (0,0) provided by {@link com.github.sef24sp4.common.vector.Coordinates}.
 	 *
-	 * @param coordinates define where on the map the item should spawn.
-	 * @param entityManager the games entity manager.
-	 * @see CommonItem
+	 * @return Returns the Item.
 	 */
-	public void spawnItem(IVector coordinates, IEntityManager entityManager);
+	public CommonItem getItem();
 
 	/**
-	 * Despawns/removes the item from the map.
-	 * Can be used if an item isn't picked up after a duration of time.
-	 * @param entityManager the games entity manager.
+	 *
+	 * @return Returns the rarity of the item from ItemRarity enum.
 	 */
-	public void despawnItem(IEntityManager entityManager);
+	public ItemRarity getRarity();
 }
