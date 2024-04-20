8082e4ee0be49ac554c67019cc91f59741aaa421
alkor22 <alkor22@student.sdu.dk>
2024-04-20 14:27:27 +0200

    Changed HealthItem
    - Modified HealthItem because change of ItemSPI

--- HealthItem/src/main/java/com/github/sef24sp4/healthitem/HealthItem.java
+++ HealthItem/src/main/java/com/github/sef24sp4/healthitem/HealthItem.java
@@ -1,41 +1,51 @@
 package com.github.sef24sp4.healthitem;
 
+
 import com.github.sef24sp4.common.entities.ICollidableEntity;
 import com.github.sef24sp4.common.entities.IHealingEntity;
 import com.github.sef24sp4.common.interfaces.IEntityManager;
 import com.github.sef24sp4.common.item.CommonItem;
+import com.github.sef24sp4.common.item.ItemRarity;
 import com.github.sef24sp4.common.item.ItemSPI;
 import com.github.sef24sp4.common.vector.Coordinates;
 import com.github.sef24sp4.player.Player;
 
-import java.util.Optional;
-
 public class HealthItem extends CommonItem implements ItemSPI, IHealingEntity {
 	private final double healAmount = 10;
-	protected HealthItem() {
+	private  final  ItemRarity rarity = ItemRarity.COMMON;
+	public HealthItem() {
+		this.setRotation(3.14159265);
+		this.setPolygonCoordinates(
+				new Coordinates(0, -5),
+				new Coordinates(6, 1),
+				new Coordinates(5, 4),
+				new Coordinates(2, 5),
+				new Coordinates(0, 2),
+				new Coordinates(-2, 5),
+				new Coordinates(-5, 4),
+				new Coordinates(-6, 1)
+		);
 	}
 
 	@Override
 	public void collide(IEntityManager entityManager, ICollidableEntity otherEntity) {
 		if (otherEntity instanceof Player) {
-			despawnItem(entityManager);
+			entityManager.removeEntity(this);
 		}
-	}
-
-	@Override
-	public void spawnItem(Coordinates coordinates, IEntityManager entityManager) {
-		this.setCoordinates(coordinates);
-		HealthItem healthItem = new HealthItem();
-		entityManager.addEntity(healthItem);
-	}
-
-	@Override
-	public void despawnItem(IEntityManager entityManager) {
-		entityManager.removeEntity(this);
 	}
 
 	@Override
 	public double getHealingAmount() {
 		return this.healAmount;
+	}
+
+	@Override
+	public CommonItem getItem() {
+		return this;
+	}
+
+	@Override
+	public ItemRarity getRarity() {
+		return rarity;
 	}
 }
--- HealthItem/src/main/java/com/github/sef24sp4/healthitem/HealthItemPlugin.java
+++ HealthItem/src/main/java/com/github/sef24sp4/healthitem/HealthItemPlugin.java
@@ -4,6 +4,7 @@ import com.github.sef24sp4.common.entities.IEntity;
 import com.github.sef24sp4.common.interfaces.IEntityManager;
 import com.github.sef24sp4.common.interfaces.IGameSettings;
 import com.github.sef24sp4.common.services.IGamePluginService;
+import com.github.sef24sp4.common.vector.Coordinates;
 
 public class HealthItemPlugin implements IGamePluginService {
 	@Override
@@ -13,13 +14,19 @@ public class HealthItemPlugin implements IGamePluginService {
 
 	@Override
 	public void gameStart(IEntityManager entityManager, IGameSettings gameSettings) {
-
+		int x = gameSettings.getDisplayWidth()/2;
+		int y = gameSettings.getDisplayHeight()/2;
+		HealthItem healthItem = new HealthItem();
+		healthItem.setX(x);
+		healthItem.setY(y);
+		entityManager.addEntity(healthItem);
+		System.out.println("Health item added " + healthItem.getRarity());
 	}
 
 	@Override
 	public void gameStop(IEntityManager entityManager, IGameSettings gameSettings) {
-		for (IEntity HealthItem:entityManager.getEntitiesByClass(HealthItem.class)) {
-			entityManager.removeEntity(HealthItem);
+		for (IEntity healthItem:entityManager.getEntitiesByClass(HealthItem.class)) {
+			entityManager.removeEntity(healthItem);
 		}
 	}
 }
