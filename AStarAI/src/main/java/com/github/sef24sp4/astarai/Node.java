953b7042b5f5272fbb387525a4138cc9f241ed0a
MartinLundDam <martinlunddam@hotmail.dk>
2024-04-24 22:21:56 +0200

    Completion of the OpenNode, and edits of the nextCoordinateInPath() method

--- AStarAI/src/main/java/com/github/sef24sp4/astarai/Node.java
+++ AStarAI/src/main/java/com/github/sef24sp4/astarai/Node.java
@@ -5,14 +5,10 @@ import com.github.sef24sp4.common.ai.map.MapNode;
 import com.github.sef24sp4.common.vector.IVector;
 
 import java.util.Collection;
-import java.util.Optional;
 
 public class Node {
 	private Node parent;
-	private int x;
-	private int y;
 	private double gCost;
-	private double hCost;
 	private double fCost;
 	private final IVector coordinates;
 
@@ -24,15 +20,12 @@ public class Node {
 	}
 
 	public Collection<Node> getNeighboringNodes() {
-		return this.mapNode.getNeighboringNodes().stream().map(mapNode1 -> new Node(coordinates, mapNode1)).toList();
+		return this.mapNode.getNeighboringNodes().stream().map(mapNode1 -> new Node(this.coordinates, mapNode1)).toList();
 
 	}
 
 	public double getHeuristicForNode(Node targetNode) {
 		return this.mapNode.calculateHeuristicsFor(targetNode.getMapNode());
-	}
-	public Optional<IVector> getSafeCoordinatesForEntity(){
-		mapNode.getSafeCoordinatesForEntity()
 	}
 
 	public IVector getCoordinates() {
@@ -47,68 +40,23 @@ public class Node {
 		return this.getMapNode().equals(node.getMapNode());
 	}
 
-	public int getX() {
-		return this.x;
-	}
-
-	public int getY() {
-		return this.y;
-	}
 
 	public double getGCost() {
 		return this.gCost;
 	}
 
-	public double getHCost() {
-		return this.hCost;
-	}
 
 	public double getFCost() {
 		return this.fCost;
 	}
 
-	public boolean isSolid() {
-		return this.solid;
-	}
-
-	public boolean isOpen() {
-		return this.open;
-	}
-
-	public boolean isChecked() {
-		return this.checked;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
 
 	public void setGCost(double cost) {
 		this.gCost = cost;
 	}
 
-	public void setHCost(double cost) {
-		this.hCost = cost;
-	}
-
 	public void setFCost(double cost) {
 		this.fCost = cost;
-	}
-
-	public void setSolid(boolean solid) {
-		this.solid = solid;
-	}
-
-	public void setOpen(boolean open) {
-		this.open = open;
-	}
-
-	public void setChecked(boolean checked) {
-		this.checked = checked;
 	}
 
 	public Node getParent() {
--- CommonAI/src/main/java/com/github/sef24sp4/common/ai/IPathfindingProvider.java
+++ CommonAI/src/main/java/com/github/sef24sp4/common/ai/IPathfindingProvider.java
@@ -1,7 +1,6 @@
 package com.github.sef24sp4.common.ai;
 
 import com.github.sef24sp4.common.entities.ICollidableEntity;
-import com.github.sef24sp4.common.entities.IEntity;
 import com.github.sef24sp4.common.vector.IVector;
 
 public interface IPathfindingProvider {
