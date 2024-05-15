b76406f72bff8198b5029e9c20d46da09bd69ab1
vimis22 <vimis22@student.sdu.dk>
2024-05-15 12:45:53 +0200

    Additional changes made to module-info.java in the WeaponPack.

--- WeaponPack/src/main/java/module-info.java
+++ WeaponPack/src/main/java/module-info.java
@@ -2,13 +2,9 @@ import com.github.sef24sp4.common.item.ItemSPI;
 import com.github.sef24sp4.common.projectile.ProjectileSPI;
 import com.github.sef24sp4.common.services.IEntityProcessingService;
 import com.github.sef24sp4.common.services.IGamePluginService;
-import com.github.sef24sp4.common.weapon.WeaponSPI;
-import com.github.sef24sp4.weaponpack.AbstractBulletControlSystem;
 import com.github.sef24sp4.weaponpack.machinegun.MachineGunBulletControlSystem;
-import com.github.sef24sp4.weaponpack.machinegun.MachineGun;
 import com.github.sef24sp4.weaponpack.BulletPlugin;
 import com.github.sef24sp4.weaponpack.machinegun.MachineGunItemProvider;
-import com.github.sef24sp4.weaponpack.shotgun.ShotGun;
 import com.github.sef24sp4.weaponpack.shotgun.ShotGunBulletControlSystem;
 import com.github.sef24sp4.weaponpack.shotgun.ShotGunItemProvider;
 
@@ -18,7 +14,6 @@ module WeaponPack {
 	requires CommonWeapon;
 	requires CommonItem;
 	provides IGamePluginService with BulletPlugin;
-	provides WeaponSPI with MachineGun, ShotGun;
 	provides ItemSPI with ShotGunItemProvider, MachineGunItemProvider;
 	provides ProjectileSPI with MachineGunBulletControlSystem, ShotGunBulletControlSystem;
 	provides IEntityProcessingService with MachineGunBulletControlSystem, ShotGunBulletControlSystem;
