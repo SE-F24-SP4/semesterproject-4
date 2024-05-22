SE-F24-SP4/semesterproject-4

commit 33907165fb737ca33462a53ef8d968e0f7dbbf6a

Implement game stopping when player dies. (#125)

Core/src/main/java/com/github/sef24sp4/core/scenes/GameScene.java
@@ -19,28 +19,37 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 
 public final class GameScene {
+	private static Scene scene;
+	private static IGameProcessor gameProcessor;
+
 	private GameScene() {
 	}
 
-	public static void load(final Scene scene) {
+	public static void load(final Scene rootScene) {
+		GameScene.scene = rootScene;
+
 		final Button button = new Button("Back to start");
 		final Pane pane = new Pane(button);
-		scene.setRoot(pane);
+		GameScene.scene.setRoot(pane);
 
 		JavaFxLabelProvider.setRoot(pane);
 
-		final IGameProcessor gameProcessor = GameScene.getGameProcessor(pane);
+		gameProcessor = GameScene.getGameProcessor(pane);
 
 		button.addEventHandler(ActionEvent.ANY, event -> {
-			try {
-				gameProcessor.end();
-				StartMenu.load(scene);
-			} catch (IOException e) {
-				throw new UncheckedIOException(e);
-			}
 		});
 
 		gameProcessor.begin();
+	}
+
+	private static boolean endGame() {
+		try {
+			gameProcessor.end();
+			StartMenu.load(scene);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+		return true;
 	}
 
 	public static IGameProcessor getGameProcessor(final Pane pane) {
@@ -49,6 +58,8 @@ public final class GameScene {
 		gameSettings.setDisplayWidth(Math.toIntExact(Math.round(pane.getWidth())));
 
 		GameSettingsProvider.setSettings(gameSettings);
+
+		gameSettings.setEndGameCallback(GameScene::endGame);
 
 		pane.widthProperty().addListener(((observable, oldValue, newValue) -> gameSettings.setDisplayWidth(newValue.intValue())));
 		pane.heightProperty().addListener(((observable, oldValue, newValue) -> gameSettings.setDisplayHeight(newValue.intValue())));

Common/src/main/java/com/github/sef24sp4/common/interfaces/IGameSettings.java
@@ -17,4 +17,12 @@ public interface IGameSettings {
 				&& entity.getX() <= this.getDisplayWidth()
 				&& entity.getY() <= this.getDisplayHeight();
 	}
+
+	/**
+	 * Attempt to stop the current game.
+	 *
+	 * @return {@code true} if the game is stopped successfully.
+	 * {@code false} otherwise.
+	 */
+	public boolean stopGame();
 }

Core/src/main/java/com/github/sef24sp4/core/game/GameSettings.java
@@ -2,10 +2,14 @@ package com.github.sef24sp4.core.game;
 
 import com.github.sef24sp4.common.interfaces.IGameSettings;
 
+import java.util.function.BooleanSupplier;
+
 public class GameSettings implements IGameSettings {
 	private final GameInput gameInput;
 	private int displayWidth;
 	private int displayHeight;
+
+	private BooleanSupplier endGameCallback;
 
 	public GameSettings(final GameInput gameInput) {
 		this.gameInput = gameInput;
@@ -32,5 +36,15 @@ public class GameSettings implements IGameSettings {
 
 	public void setDisplayHeight(final int displayHeight) {
 		this.displayHeight = displayHeight;
+	}
+
+	@Override
+	public boolean stopGame() {
+		if (this.endGameCallback == null) return false;
+		return this.endGameCallback.getAsBoolean();
+	}
+
+	public void setEndGameCallback(final BooleanSupplier endGameCallback) {
+		this.endGameCallback = endGameCallback;
 	}
 }

Player/src/main/java/com/github/sef24sp4/player/Player.java
@@ -1,6 +1,7 @@
 package com.github.sef24sp4.player;
 
 import com.github.sef24sp4.common.entities.IHealingEntity;
+import com.github.sef24sp4.common.interfaces.GameSettingsLoader;
 import com.github.sef24sp4.common.item.itemtypes.ISpeedItem;
 import com.github.sef24sp4.common.item.itemtypes.WeaponItem;
 import com.github.sef24sp4.common.vector.Coordinates;
@@ -96,6 +97,7 @@ public final class Player extends CommonEntity implements ICollidableEntity {
 	 */
 	void kill(IEntityManager entityManager) {
 		entityManager.removeEntity(this);
+		GameSettingsLoader.load().stopGame();
 	}
 
 	/**

Player/src/test/java/com/github/sef24sp4/player/PlayerTest.java
@@ -2,21 +2,25 @@ package com.github.sef24sp4.player;
 
 import com.github.sef24sp4.common.entities.IAttackingEntity;
 import com.github.sef24sp4.common.entities.ICollidableEntity;
+import com.github.sef24sp4.common.interfaces.GameSettingsLoader;
 import com.github.sef24sp4.common.interfaces.IEntityManager;
+import com.github.sef24sp4.common.interfaces.IGameSettings;
 import com.github.sef24sp4.common.item.itemtypes.WeaponItem;
 import com.github.sef24sp4.common.projectile.CommonProjectile;
 import com.github.sef24sp4.common.weapon.WeaponSPI;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PlayerTest {
@@ -24,6 +28,14 @@ public class PlayerTest {
 
 	@Mock
 	private IEntityManager mockEntityManager;
+
+	private static void runWithMockedGameSettings(final Executable executor) throws Throwable {
+		try (MockedStatic<GameSettingsLoader> mockedGameSettings = mockStatic(GameSettingsLoader.class)) {
+			mockedGameSettings.when(GameSettingsLoader::load).thenReturn(mock(IGameSettings.class));
+			executor.execute();
+			mockedGameSettings.verify(GameSettingsLoader::load); // If this fails then GameSettings is not needed. Run without this method.
+		}
+	}
 
 	@BeforeEach
 	void setUp() throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
@@ -83,16 +95,20 @@ public class PlayerTest {
 	}
 
 	@Test
-	void kill() {
+	void kill() throws Throwable {
 		this.mockEntityManager.addEntity(this.player);
-		this.player.kill(this.mockEntityManager);
+
+		runWithMockedGameSettings(() -> this.player.kill(this.mockEntityManager));
+
 		verify(this.mockEntityManager).removeEntity(this.player);
 	}
 
 	@Test
-	void takeDamageAndKill() {
+	void takeDamageAndKill() throws Throwable {
 		this.mockEntityManager.addEntity(this.player);
-		this.player.takeDamage(1000, this.mockEntityManager);
+
+		runWithMockedGameSettings(() -> this.player.takeDamage(1000, this.mockEntityManager));
+
 		verify(this.mockEntityManager).removeEntity(this.player);
 	}
 
@@ -101,7 +117,7 @@ public class PlayerTest {
 		//Setup
 		ICollidableEntity mockCollidableEntity = mock(ICollidableEntity.class, withSettings().extraInterfaces(IAttackingEntity.class));
 		IAttackingEntity mockAttackingEntity = (IAttackingEntity) mockCollidableEntity;
-		when(mockAttackingEntity.getAttackDamage()).thenReturn(10.0);
+		when(mockAttackingEntity.getAttackDamage()).thenReturn(1.0);
 
 		//Method call
 		this.player.collide(this.mockEntityManager, mockCollidableEntity);

commit 58ad1dbc1d2379651c9c2bd029682e219add34c4

Fix back button not working
Regression introduced in 33907165fb737ca33462a53ef8d968e0f7dbbf6a

Core/src/main/java/com/github/sef24sp4/core/scenes/GameScene.java
@@ -37,6 +37,7 @@ public final class GameScene {
 		gameProcessor = GameScene.getGameProcessor(pane);
 
 		button.addEventHandler(ActionEvent.ANY, event -> {
+			endGame();
 		});
 
 		gameProcessor.begin();